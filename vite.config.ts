b7dabfed8ca1c4f1630ad64f26883bd57806bc9a
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-03-01 13:31:26 +0700

    feat: setup linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    react(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "**/*.{mjs,ts,tsx}"',
+        useFlatConfig: true,
+      },
+      typescript: true,
+    }),
+  ],
+});
